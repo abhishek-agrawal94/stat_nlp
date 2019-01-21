c2ed44c5ca797bb5cf3553c4b5fd64ffc12e89e8
abhishek <abhi1994.aa@gmail.com>
2019-01-21 03:03:49 -0500

    added changes

--- em.py
+++ em.py
@@ -1,37 +1,74 @@
 from collections import Counter, defaultdict
+import random
 
-def get_word_prob(file_name):
-	total_count = sum(1 for line in open(file_name))
-	#print("total count: {}".format(total_count))
-    total_count = 0
-    vocab_size = 0
-    vocab = set()
-	c= Counter()
+def get_probs(file_name):
 	with open(file_name) as f:
-		for line in f:
-            total_count += 1
-            vocab.add(line.split()[0])
-			c.update(line.split())
-    vocab_size = len(vocab)
-	#print(c)
-	word_count = dict(c)
-	#print("word count: {}".format(word_count))
+		text = f.read()
+	tokens = text.split()
+	total_count = len(tokens)
+	vocab = set(tokens)
+	unigrams = Counter(tokens)
+	bigram_text = "<s>\n" + text
+	bigram_tokens = bigram_text.split()
+	bigrams = zip(bigram_tokens,bigram_tokens[1:])
+	bigram_count = Counter(bigrams)
+	trigram_text = "<s>\n" + bigram_text
+	trigram_tokens = trigram_text.split()
+	trigrams = zip(trigram_tokens,trigram_tokens[1:],trigram_tokens[2:])
+	trigram_count = Counter(trigrams)
+	p0 = 1 / len(vocab)
+	p1 = unigrams
+	for unigram in unigrams:
+		p1[unigram] = unigrams[unigram] / total_count
+	p2 = bigram_count
+	for bigram in bigram_count:
+		if bigram[0] in unigrams:
+			p2[bigram] = bigram_count[bigram] / unigrams[bigram[0]]
+	p3 = trigram_count
+	for trigram in trigram_count:
+		if (trigram[0], trigram[1]) in bigrams:
+			p3[trigram] = trigram_count[trigram] / bigram_count[trigram[0],trigram[1]]
+	return p0, p1, p2, p3
 
-	word_prob = {}
-	for key, value in word_count.items():
-		word_prob[key] = value/total_count
-	return word_prob, word_count, 
-	#print("word probability: {}".format("word_prob"))
-
-def get_bigram_prob(file_name):
-	bigram_count = defaultdict(int)
-	total_bigrams = 0
-	previous = ""
+def calc_lambdas(file_name, p0, p1, p2, p3):
 	with open(file_name) as f:
-		for line in f:
-			if(previous != ""):
-				bigram_count[previous + "," + line.split()[0]] += 1
-				total_bigrams += 1
-			previous = line.split()[0]
-	return bigram_count, total_bigrams
-	#print("bigram count: {}".format(bigram_count))
+		text = f.read()
+	text = "<s>\n<s>\n" + text
+	tokens = text.split()
+	trigrams = list(zip(tokens,tokens[1:], tokens[2:]))
+	termination_condition = 0.0001
+	lambdas = []
+	for i in range(4):
+		lambdas.append(random.randint(1,10))
+	norm = sum(lambdas)
+	for i in range(4):
+		lambdas[i] /= norm
+	next_lambdas = [0.0] * 4
+	expected_counts = [0.0] * 4
+	while True:
+		for trigram in trigrams:
+			prob = lambdas[3] * p3[trigram] + lambdas[2] * p2[trigram[1], trigram[2]] + lambdas[1] * p1[trigram[2]] + lambdas[0] * p0
+			expected_counts[0] += lambdas[0] * p0 / prob
+			expected_counts[1] += lambdas[1] * p1[trigram[2]] / prob
+			expected_counts[2] += lambdas[2] * p2[trigram[1], trigram[2]] / prob
+			expected_counts[3] += lambdas[3] * p3[trigram] / prob
+		arr = []
+		for i in range(4):
+			next_lambdas[i] = expected_counts[i] / sum(expected_counts)
+			arr.append(abs((lambdas[i] - next_lambdas[i])) < termination_condition)
+
+		lambdas = next_lambdas.copy()
+		expected_counts = [0.0] * 4
+		if all(arr):
+			break
+	print("Smoothed lambdas:")
+	print(lambdas)
+
+
+
+p0, p1, p2, p3 = get_probs("TRAINEN.txt")
+lambdas = calc_lambdas("HOLDOUTEN.txt", p0, p1, p2, p3)
+#word_prob, word_count, uniform_probability = get_word_prob(file_name)
+#get_bigram_prob(file_name, word_count)
+#bigram_count, bigram_probability = get_bigram_prob(file_name, word_count)
+#trigram_count, trigram_probability = get_trigram_prob(file_name, bigram_count)
--- entropy.py
+++ entropy.py
@@ -1,7 +1,7 @@
 from collections import Counter, defaultdict
 import math, random
 
-def randomize_character(file_name,res_file):
+def randomize_character(file_name,res_file, mess_probability):
 	character_list = set()
 	with open(file_name) as f:
 		for line in f:
@@ -17,13 +17,15 @@ def randomize_character(file_name,res_file):
 			word = ""
 			for char in line.split()[0]:
 				random.seed()
-				if(random.random() <= 0.0001):
+				if(random.random() <= mess_probability):
 					char = random.choice(list(character_list))
 				word += char
 			write_file.write(word + "\n")
 	write_file.close()
+	entropy, perplexity = get_cond_prob_and_perplexity(res_file)
+	print("After messing up characters with a probability of {0} for file {1}, the entropy is: {2}".format(mess_probability, file_name, entropy))
 
-def randomize_word(file_name,res_file):
+def randomize_word(file_name,res_file, mess_probability):
 	word_list = set()
 	with open(file_name) as f:
 		for line in f:
@@ -36,76 +38,55 @@ def randomize_word(file_name,res_file):
 		for line in f:
 			word = line.split()[0]
 			random.seed()
-			if(random.random() <= 0.1):
+			if(random.random() <= mess_probability):
 				word = random.choice(list(word_list))
 			write_file.write(word + "\n")
 	write_file.close()
+	entropy, perplexity = get_cond_prob_and_perplexity(res_file)
+	print("After messing up words with a probability of {0} for file {1}, the entropy is: {2}".format(mess_probability, file_name, entropy))
 
-def get_word_prob(file_name):
-	total_count = sum(1 for line in open(file_name))
-	#print("total count: {}".format(total_count))
+def get_cond_prob_and_perplexity(file_name):
+	text = open(file_name).read()
+	lines = text.split("\n")
+	total_count = len(lines)
+	unigram = defaultdict(int)
+	for line in lines:
+		unigram[line] += 1
 
-	c= Counter()
-	with open(file_name) as f:
-		for line in f:
-			c.update(line.split())
-
-	#print(c)
-	word_count = dict(c)
-	#print("word count: {}".format(word_count))
-
-	word_prob = {}
-	for key, value in word_count.items():
-		word_prob[key] = value/total_count
-	return word_prob
-	#print("word probability: {}".format("word_prob"))
-
-def get_bigram_prob(file_name):
-	bigram_count = defaultdict(int)
-	total_bigrams = 0
-	previous = ""
-	with open(file_name) as f:
-		for line in f:
-			if(previous != ""):
-				bigram_count[previous + "," + line.split()[0]] += 1
-				total_bigrams += 1
-			previous = line.split()[0]
-	return bigram_count, total_bigrams
-	#print("bigram count: {}".format(bigram_count))
-
-def get_entropy_perplexity(file_name, word_prob, bigram_count, total_bigrams, run):
-	previous = ""
-	joint_probability = {}
-	conditional_probability = {}
-	summation = 0
-	entropy = 0
-	perplexity = 0
-	with open(file_name) as f:
-		for line in f:
-			if(previous != ""):
-				#joint_probability[previous + "," + line.split()[0]] = word_prob[previous] * word_prob[line.split()[0]]
-				joint_probability[previous + "," + line.split()[0]] = bigram_count[previous + "," + line.split()[0]] / total_bigrams
-				conditional_probability[line.split()[0] + "|" + previous] = joint_probability[previous + "," + line.split()[0]] / word_prob[previous]
-				summation += joint_probability[previous + "," + line.split()[0]] * math.log(conditional_probability[line.split()[0] + "|" + previous], 2)
-			previous = line.split()[0]
-	#print("Joint probability: {}".format(joint_probability))
-	#print("Conditional probability: {}".format(conditional_probability))
-	entropy = (-1) * summation
-	print(run)
-	print("Entropy: {}".format(entropy))
+	bigrams = Counter(zip(lines, lines[1:]))
+	sum = 0.0
+	bigram_count = total_count - 1
+	for word in bigrams:
+		if word[0] in unigram:
+			joint_prob = float(bigrams[word]/bigram_count)
+			cond_prob = float(bigrams[word]/unigram[word[0]])
+			sum += joint_prob * math.log(cond_prob, 2)
+	entropy = -sum
 	perplexity = math.pow(2, entropy)
-	print("perplexity: {}".format(perplexity))
+	return entropy, perplexity
 
+def mess_up_experiments(file, res_file):
+	for i in range(10):
+		randomize_character(file, res_file, 0.1)
+		randomize_character(file, res_file, 0.05)
+		randomize_character(file, res_file, 0.01)
+		randomize_character(file, res_file, 0.001)
+		randomize_character(file, res_file, 0.0001)
+		randomize_character(file, res_file, 0.00001)
+		randomize_word(file, res_file, 0.1)
+		randomize_word(file, res_file, 0.05)
+		randomize_word(file, res_file, 0.01)
+		randomize_word(file, res_file, 0.001)
+		randomize_word(file, res_file, 0.0001)
+		randomize_word(file, res_file, 0.00001)
 
+entropy, perplexity = get_cond_prob_and_perplexity("TEXTEN1.txt")
+print("The entropy for the English text is", entropy)
+print("The perplexity for the English text is", perplexity)
+entropy, perplexity = get_cond_prob_and_perplexity("TEXTCZ1UTF.txt")
+print("The entropy for the Czech text is", entropy)
+print("The perplexity for the Czech text is", perplexity)
 
-
-file_name = "TEXTCZ1UTF.txt"
-word_prob = get_word_prob(file_name)
-bigram_count, total_bigrams = get_bigram_prob(file_name)
-get_entropy_perplexity(file_name, word_prob, bigram_count, total_bigrams, "original: ")
 res_file = "random_text.txt"
-for i in range(10):
-	randomize_word(file_name, res_file)
-	word_prob = get_word_prob(res_file)
-	bigram_count, total_bigrams = get_bigram_prob(res_file)
-	get_entropy_perplexity(res_file, word_prob, bigram_count, total_bigrams, "run " + str(i) + " :")
+mess_up_experiments("TEXTEN1.txt",res_file)
+mess_up_experiments("TEXTCZ1UTF",res_file)
